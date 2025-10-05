91c75d6a1e4d7ed8f01cdb6ac3a41b0d64479e15
Yashraj Deshmukh <dyashraj717@gmail.com>
2025-10-06 02:30:17 +0530

    done configuring brevo

--- backend/src/main/java/com/example/Dailydone/Service/EmailService.java
+++ backend/src/main/java/com/example/Dailydone/Service/EmailService.java
@@ -13,7 +13,7 @@ public class EmailService {
     public void sendOtp(String toEmail, String otp) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
-        message.setFrom("otp@dailydone.com"); // your email
+        message.setFrom("dyashraj717@gmail.com"); // your email
         message.setSubject("Your DailyDone OTP");
         message.setText("Your OTP is: " + otp);
         javaMailSender.send(message);
