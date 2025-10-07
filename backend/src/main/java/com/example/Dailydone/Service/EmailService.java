1770da34af3dc910ae94290ca779fe533e8f3347
Yashraj Deshmukh <dyashraj717@gmail.com>
2025-10-07 23:40:25 +0530

    added brevo api

--- backend/src/main/java/com/example/Dailydone/Service/EmailService.java
+++ backend/src/main/java/com/example/Dailydone/Service/EmailService.java
@@ -1,21 +1,53 @@
 package com.example.Dailydone.Service;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import okhttp3.RequestBody;
 
 @Service
 public class EmailService {
-    @Autowired
-    private JavaMailSender javaMailSender;
+
+    @Value("${BREVO_API_KEY}")
+    private String brevoApiKey;
 
     public void sendOtp(String toEmail, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setFrom("dyashraj717@gmail.com"); // your email
-        message.setSubject("Your DailyDone OTP");
-        message.setText("Your OTP is: " + otp);
-        javaMailSender.send(message);
+        OkHttpClient client = new OkHttpClient();
+
+        String json = """
+        {
+          "sender": {"name": "DailyDone", "email": "dailydonehelp@gmail.com"},
+          "to": [{"email": "%s"}],
+          "subject": "Your DailyDone OTP",
+          "htmlContent": "<p>Your OTP is <b>%s</b></p>"
+        }
+        """.formatted(toEmail, otp);
+
+        RequestBody body = RequestBody.create(
+                json,
+                MediaType.parse("application/json")
+        );
+        Request request = new Request.Builder()
+                .url("https://api.brevo.com/v3/smtp/email")
+                .post(body)
+                .addHeader("accept", "application/json")
+                .addHeader("api-key", brevoApiKey)
+                .addHeader("content-type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("Brevo response code: " + response.code());
+            if (!response.isSuccessful()) {
+                System.err.println("Brevo failed: " + response.body().string());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
